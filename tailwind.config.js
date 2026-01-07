ad4c88c62435c29bdc94570bacc69adb8dc701c8
venkata sai prasad muddada <s.muddada@ajacs.in>
2026-01-07 12:48:02 +0530

    onboarding v1

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './accounts/**/*.html',
+    './candidate/**/*.html',
+    './documents/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
