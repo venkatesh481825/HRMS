354ac16a3094c2ee19e04b6b6c3f82e8e113ee3e
venkatasaiprasad2001 <s.muddada@ajacs.in>
2026-01-07 12:28:55 +0530

    Delete candidate directory

--- candidate/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import create_candidate, candidate_onboard
-
-urlpatterns = [
-    path('create/', create_candidate),
-    path('onboard/<uuid:token>/', candidate_onboard),
-]
