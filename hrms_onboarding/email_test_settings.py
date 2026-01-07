d276305e7be8d4abcff093f533a8182ce68e634a
venkatasaiprasad2001 <s.muddada@ajacs.in>
2026-01-07 12:29:34 +0530

    Delete hrms_onboarding directory

--- hrms_onboarding/email_test_settings.py
+++
@@ -1,9 +0,0 @@
-# Temporary email settings for testing
-# To use console backend (prints emails to console instead of sending):
-# Change EMAIL_BACKEND in settings.py to:
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-# Or use file backend (saves emails to files):
-# EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-# EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
-
