91fc0db8ffbbe7e53ea4441de8c4b1f4c8236a31
venkatasaiprasad2001 <s.muddada@ajacs.in>
2026-01-07 12:29:15 +0530

    Delete accounts directory

--- accounts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile
-
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'role')
-    list_filter = ('role',)
